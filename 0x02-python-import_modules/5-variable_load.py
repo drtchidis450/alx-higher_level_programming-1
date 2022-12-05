3f2d9ccb084c1d90596bb866f6b174e8f7cc07c2
Alfenkeast <110989525+Alfenkeast@users.noreply.github.com>
2022-12-05 16:52:05 +0100

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4
+    from variable_load_5 import a
 
-    names = dir(hidden_4)
-
-    for name in names:
-        if "__" not in name[:2]:
-            print("{:s}".format(name))
+    print("{:d}".format(a))
