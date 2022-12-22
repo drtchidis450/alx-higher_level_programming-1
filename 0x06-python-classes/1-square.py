1fdadfd81aed63cac95370bdccb366d0edd34c5a
Alfenkeast <110989525+Alfenkeast@users.noreply.github.com>
2022-12-22 02:11:20 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+# 0-square.py by Chidi Ojike
+"""Defines a square """
+
+
 class Square:
-    """Defines a square"""
+    """Represents a square"""
+
     def __init__(self, size):
-        """Initialises the data"""
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
         self.__size = size
